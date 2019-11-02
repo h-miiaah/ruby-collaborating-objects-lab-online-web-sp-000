1f16e66724ab61534bd3fed40335b97756aba5e1
Haim D <haim11293@aol.com>
2019-11-02 19:45:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,4 +6,17 @@ class Song
   def initialize(name)
     @name = name
   end
+
+  def self.new_by_filename(filename)
+    song = filename.split(" - ")[1]
+    artist = filename.split(" - ")[0]
+    new_song = self.new(song)
+    new_artist = self.new(artist)
+    new_song
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
 end
